0a6dabbe63c870df324db2cf580d1936e8be6403
Robert Damoc <robert.damoc@3pillarglobal.com>
2019-02-13 16:03:11 +0200

    Mark winning lines with red bg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const calculateWinner = (squares) => {
+const winningLine = (squares) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,7 +16,7 @@ const calculateWinner = (squares) => {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-      return squares[a];
+      return lines[i];
     }
   }
   return null;
@@ -24,7 +24,10 @@ const calculateWinner = (squares) => {
 
 const Square = (props) => {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button
+      className={'square' + (props.isWinCondition ? ' marked' : '')}
+      onClick={props.onClick}
+    >
       {props.value}
     </button>
   );
@@ -36,10 +39,11 @@ class Board extends React.Component {
     xIsNext: true,
   };
 
-  renderSquare(i) {
+  renderSquare(i, isWinCondition) {
     return (
     <Square
       value={this.state.squares[i]}
+      isWinCondition={isWinCondition}
       onClick={() => this.handleSquareClick(i)}
     />);
   }
@@ -51,7 +55,7 @@ class Board extends React.Component {
   handleSquareClick = (i) => {
     const squares = this.state.squares.slice();
 
-    if (calculateWinner(squares) || squares[i]) { return; }
+    if (winningLine(squares) || squares[i]) { return; }
 
     squares[i] = this.currentPlayerLabel();
 
@@ -61,35 +65,32 @@ class Board extends React.Component {
     });
   }
 
-  status = () => {
-    const winner = calculateWinner(this.state.squares);
-    if (winner) {
-      return 'Winner: ' + winner;
-    } else {
-      return 'Next player: ' + this.currentPlayerLabel();
-    }
-  };
-
   render() {
-    let status = this.status();
+    const line = winningLine(this.state.squares);
+    let status;
+    if (line) {
+      status = 'Winner: ' + this.state.squares[line[0]];
+    } else {
+      status = 'Next player: ' + this.currentPlayerLabel();
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0, line && line.indexOf(0) >= 0)}
+          {this.renderSquare(1, line && line.indexOf(1) >= 0)}
+          {this.renderSquare(2, line && line.indexOf(2) >= 0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3, line && line.indexOf(3) >= 0)}
+          {this.renderSquare(4, line && line.indexOf(4) >= 0)}
+          {this.renderSquare(5, line && line.indexOf(5) >= 0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6, line && line.indexOf(6) >= 0)}
+          {this.renderSquare(7, line && line.indexOf(7) >= 0)}
+          {this.renderSquare(8, line && line.indexOf(8) >= 0)}
         </div>
       </div>
     );
