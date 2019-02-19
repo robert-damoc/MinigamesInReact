422a734377682dfdaae1a4b3bf4024d9a84cabe9
Robert Damoc <robert.damoc@3pillarglobal.com>
2019-02-19 16:52:17 +0200

    Add some basic logic for Connect4

--- src/screens/Connect4/components/Board.jsx
+++ src/screens/Connect4/components/Board.jsx
@@ -1,27 +1,136 @@
 import Board from '../../../components/Board/';
 
+import './Board.css'
+
 export default class Connect4Board extends Board {
+  initState = () => ({
+    squares: Array(this.props.rows * this.props.cols).fill(null),
+    currentPlayerIndex: 0,
+    lastUsedIndex: null,
+  });
+
   handleSquareClick = (i) => {
-    console.log(i);
-    console.log(i % 7);
-    // const squares = this.state.squares.slice();
+    // if (this.gameOver()) { return; }
+    // console.log(this.gameOver());
 
-    // if (this.winningLine() || squares[i] || this.draw()) { return; }
+    let freeIndex = this.columnFreeIndex(i % this.props.cols);
+    if (typeof freeIndex !== 'number' || this.boardIsFull()) { return; }
 
-    // squares[i] = this.currentPlayerLabel();
+    let { squares } = this.state;
+    squares[freeIndex] = this.currentPlayerLabel();
 
-    // this.setState({
-    //   squares: squares,
-    //   xIsNext: !this.state.xIsNext,
-    // });
+    this.setState(prevState => ({
+      squares: squares,
+      currentPlayerIndex: this.nextPlayer(prevState),
+      lastUsedIndex: freeIndex,
+    }));
+  }
+
+  columnFreeIndex = (colIndex) => {
+    let i, found = false;
+    for (let rowIndex = this.props.rows - 1; rowIndex >= 0; rowIndex--) {
+      i = rowIndex * this.props.cols + colIndex;
+      if (!this.state.squares[i]) {
+        found = true
+        break;
+      }
+    }
+    if (found) { return i; }
   }
 
   gameOver = () => {
-
+    return this.boardIsFull() || this.winByColumn() || this.winByRow() || this.winByDiag();
   }
 
-  isWinningCell = () => {
+  winByMainDiag = (topRow, bottomRow, leftCol, rightCol) => {
+    if (bottomRow - topRow < 3 || rightCol - leftCol < 3) { return false; }
 
+    let { squares } = this.state;
+    for (let row = topRow; row < topRow + 3; row++) {
+
+    }
+  }
+
+  winByDiag = () => {
+    const { lastUsedIndex } = this.state;
+
+    if (!lastUsedIndex) { return false; }
+
+    const { cols, rows } = this.props;
+    const currentCol = lastUsedIndex % cols;
+    const currentRow = Math.floor(lastUsedIndex / cols);
+
+    const lowerBoundCol = Math.max(currentCol - 3, 0);
+    const upperBoundCol = Math.min(currentCol + 3, cols - 1);
+    const lowerBoundRow = Math.max(currentRow - 3, 0);
+    const upperBoundRow = Math.min(currentRow + 3, rows - 1);
+
+    // Check Main Diagonal
+    let topRow = currentRow - 3;
+    let leftCol = currentCol - 3;
+    while (topRow < lowerBoundRow || leftCol < lowerBoundCol) { leftCol++; topRow++; }
+
+    let bottomRow = currentRow + 3;
+    let rightCol = currentCol + 3;
+    while (bottomRow > upperBoundRow || rightCol > upperBoundCol) { rightCol--; bottomRow--; }
+
+    if (this.winByMainDiag(topRow, bottomRow, leftCol, rightCol)) { return true; }
+
+    // Check Secondary Diagonal
+
+
+    return false;
+  }
+
+  winByRow = () => {
+    const { lastUsedIndex, squares } = this.state;
+
+    if (!lastUsedIndex) { return false; }
+
+    const { cols } = this.props;
+    const startOfRowIndex = lastUsedIndex - (lastUsedIndex % cols);
+    const endOfRowIndex = lastUsedIndex + cols - 1 - (lastUsedIndex % cols);
+
+    const lowerBound = Math.max(lastUsedIndex - 3, startOfRowIndex);
+    const upperBound = Math.min(lastUsedIndex + 3, endOfRowIndex);
+    for (let i = lowerBound; i <= upperBound - 3; i++) {
+      if (squares[i] === squares[i + 1] &&
+        squares[i] === squares[i + 2] &&
+        squares[i] === squares[i + 3]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  winByColumn = () => {
+    const { lastUsedIndex, squares } = this.state;
+    const { cols, rows } = this.props;
+
+    if (!lastUsedIndex || lastUsedIndex > (cols * (rows - 3) - 1)) { return false; }
+
+    for (let i = 0; i < 3; i++) {
+      if (squares[lastUsedIndex + i * cols] !== squares[lastUsedIndex + (i + 1) * cols]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  cellClasses = (i) => {
+    const { squares } = this.state;
+    const { players } = this.props;
+    let classNames = [];
+
+    if (squares[i] === players[0]) {
+      classNames.push('player1');
+    } else if (squares[i] === players[1]) {
+      classNames.push('player2');
+    }
+
+    return classNames;
   }
 
   render() {
