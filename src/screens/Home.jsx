4e707698141f0c9a3d19880e1af6cd2d2127301b
Robert Damoc <robert.damoc@3pillarglobal.com>
2019-02-22 11:06:02 +0200

    Add some text to the home screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,7 +4,15 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is the Home</h1>
+        <h1>Learn by Playing!</h1>
+        <p>
+          I think the best way of learning something new is by practicing;
+          and what better way to practice if not by playing and having fun!
+        </p>
+        <p>
+          This app gathers multiple games that can be played on a grid/matrix.
+        </p>
+        <h2>Have fun and enjoy!</h2>
       </div>
     );
   }
