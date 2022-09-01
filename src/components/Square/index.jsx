0a82a0bdf8dd3fcb6e9e357508dfffa38ef8c5c4
Robert Damoc <robert.damoc@sortlist.com>
2022-09-01 13:56:49 +0300

    First change

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -4,6 +4,7 @@ import './Square.scss';
 
 const Square = ({ classNames = [], value, onClick }) => {
   const classes = `col ${classNames.join(' ')}`;
+  const firstChange = 'first change here';
 
   return (
     <div className={classes} onClick={onClick}>
