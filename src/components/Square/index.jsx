123c786032ad2d68f6d2078a907408056e2a66c2
Robert Damoc <robert.damoc@sortlist.com>
2022-09-01 13:57:21 +0300

    Second change

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -4,7 +4,8 @@ import './Square.scss';
 
 const Square = ({ classNames = [], value, onClick }) => {
   const classes = `col ${classNames.join(' ')}`;
-  const firstChange = 'first change here';
+  const firstChange = 'first change here; with update';
+  const secondChange = 'second change here';
 
   return (
     <div className={classes} onClick={onClick}>
